91ac241d95b90d34b94909588ab24c894158eb84
choseheon <whtpgjs94@naver.com>
2021-08-03 22:57:29 +0900

    First Commit

---
+++ src/main/java/com/choseheon/javaspringbnd/JavaSpringBndApplication.java
@@ -0,0 +1,13 @@
+package com.choseheon.javaspringbnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBndApplication.class, args);
+	}
+
+}
